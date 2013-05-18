abbf11b6b7b89b55320dc73005b0e09aaf2a085f
xGhOsTkiLLeRx <ratchet.player@gmx.de>
2013-05-18 02:53:24 +0200

    Changed: Use coding conventions

--- src/main/java/CookMeCommands.java
+++ src/main/java/CookMeCommands.java
@@ -16,182 +16,182 @@ import java.util.Arrays;
  */
 
 public class CookMeCommands {
-	private CookMe plugin;
+    private CookMe plugin;
 
-	public CookMeCommands(CookMe instance) {
-		plugin = instance;
+    public CookMeCommands(CookMe instance) {
+	plugin = instance;
+    }
+
+    // Commands; always check for permissions!
+    public boolean onCommand (Player player, String[] args) {
+	// reload
+	if (player.canUseCommand("/cookme") || !plugin.permissions) {
+	    if (args.length > 0 && args[0].equalsIgnoreCase("reload")) {
+		CookMeReload(player);
+		return true;
+	    }
+	    // help
+	    if (args.length > 0 && args[0].equalsIgnoreCase("help")) {
+		CookMeHelp(player);	
+		return true;
+	    }
+	    else return false;
 	}
-
-	// Commands; always check for permissions!
-	public boolean onCommand (Player player, String[] args) {
-		// reload
-		if (player.canUseCommand("/cookme") || !plugin.permissions) {
-			if (args.length > 0 && args[0].equalsIgnoreCase("reload")) {
-				CookMeReload(player);
-				return true;
-			}
-			// help
-			if (args.length > 0 && args[0].equalsIgnoreCase("help")) {
-				CookMeHelp(player);	
-				return true;
-			}
-			else return false;
-		}
-		// Set cooldown, duration or percentage of an effect
-		if (args.length > 1 && args[0].equalsIgnoreCase("set")) {
-			if (args[1].equalsIgnoreCase("cooldown")) {
-				if (args.length > 2) {
-					int cooldown = 0;
-					try {
-						cooldown = Integer.valueOf(args[2]);
-					}
-					// Cooldown not a number?
-					catch (NumberFormatException e) {
-						String message = plugin.localization.getString("no_number");
-						plugin.message(player, message, null, null);
-						return true;
-					}
-					plugin.config.setInt("configuration.cooldown", cooldown);
-					plugin.config.save();
-					String message = plugin.localization.getString("changed_cooldown");
-					plugin.message(player, message, Integer.toString(cooldown), null);
-					plugin.cooldownManager.setCooldown(cooldown);
-					return true;
-				}
-				else return false;
-			}
-			else return false;
-		}
-		// Duration
-		if (args[1].equalsIgnoreCase("duration") && args.length > 2) {
-			// Max or Min
-			if (args[2].equalsIgnoreCase("max") || args[2].equalsIgnoreCase("min")) {
-				if (args.length > 3) {
-					int duration = 0;
-					try {
-						duration = Integer.valueOf(args[3]);
-					}
-					// Duration not a number?
-					catch (NumberFormatException e) {
-						String message = plugin.localization.getString("no_number");
-						plugin.message(player, message, null, null);
-						return true;
-					}
-					plugin.config.setInt("configuration.duration." + args[2].toLowerCase(), duration);
-					plugin.config.save();
-					String message = plugin.localization.getString("changed_duration_" + args[2].toLowerCase());
-					plugin.message(player, message, Integer.toString(duration), null);
-					return true;
-				}
-				else return false;
-			}
-			else return false;
-		}
-		// Effect
-		if (Arrays.asList(plugin.effects).contains(args[1].toLowerCase())) {
-			String effect = args[1].toLowerCase();
-			if (args.length > 2) {
-				double percentage = 0.0;
-				try {
-					percentage = Double.valueOf(args[2]);
-				}
-				// Percentage not a number?
-				catch (NumberFormatException e) {
-					String message = plugin.localization.getString("no_number");
-					plugin.message(player, message, null, null);
-					return true;
-				}
-				plugin.config.setDouble("effects." + effect.toLowerCase(), percentage);
-				plugin.config.save();
-				String message = plugin.localization.getString("changed_effect");
-				plugin.message(player, message, effect, Double.toString(percentage));
-				return true;
-			}
-			else return false;
-		}
-		// enable
-		if (args.length > 0 && args[0].equalsIgnoreCase("enable")) {
-			// permissions
-			if (args.length > 1 && args[1].equalsIgnoreCase("permissions")) {
-				CookMeEnablePermissions(player);
-				return true;
-			}
-			// messages
-			if (args.length > 1 && args[1].equalsIgnoreCase("messages")) {
-				CookMeEnableMessages(player);
-				return true;
-			}
-			else return false;
-		}
-		// disable
-		if (args.length > 0 && args[0].equalsIgnoreCase("disable")) {
-			// permissions
-			if (args.length > 1 && args[1].equalsIgnoreCase("permissions")) {
-				CookMeDisablePermissions(player);
-				return true;
-			}
-			// messages
-			if (args.length > 1 && args[1].equalsIgnoreCase("messages")) {
-				CookMeDisableMessages(player);
-				return true;
-			}
-			else return false;
-		}
-		else {
-			String message = plugin.localization.getString("permission_denied");
+	// Set cooldown, duration or percentage of an effect
+	if (args.length > 1 && args[0].equalsIgnoreCase("set")) {
+	    if (args[1].equalsIgnoreCase("cooldown")) {
+		if (args.length > 2) {
+		    int cooldown = 0;
+		    try {
+			cooldown = Integer.valueOf(args[2]);
+		    }
+		    // Cooldown not a number?
+		    catch (NumberFormatException e) {
+			String message = plugin.localization.getString("no_number");
 			plugin.message(player, message, null, null);
 			return true;
+		    }
+		    plugin.config.setInt("configuration.cooldown", cooldown);
+		    plugin.config.save();
+		    String message = plugin.localization.getString("changed_cooldown");
+		    plugin.message(player, message, Integer.toString(cooldown), null);
+		    plugin.cooldownManager.setCooldown(cooldown);
+		    return true;
 		}
+		else return false;
+	    }
+	    else return false;
 	}
-
-	// See the help with /cookme help
-	private void CookMeHelp(Player player) {
-		for (int i = 1; i <= 11; i++) {
-			String message = plugin.localization.getString("help_" + Integer.toString(i));
+	// Duration
+	if (args[1].equalsIgnoreCase("duration") && args.length > 2) {
+	    // Max or Min
+	    if (args[2].equalsIgnoreCase("max") || args[2].equalsIgnoreCase("min")) {
+		if (args.length > 3) {
+		    int duration = 0;
+		    try {
+			duration = Integer.valueOf(args[3]);
+		    }
+		    // Duration not a number?
+		    catch (NumberFormatException e) {
+			String message = plugin.localization.getString("no_number");
 			plugin.message(player, message, null, null);
+			return true;
+		    }
+		    plugin.config.setInt("configuration.duration." + args[2].toLowerCase(), duration);
+		    plugin.config.save();
+		    String message = plugin.localization.getString("changed_duration_" + args[2].toLowerCase());
+		    plugin.message(player, message, Integer.toString(duration), null);
+		    return true;
 		}
+		else return false;
+	    }
+	    else return false;
 	}
-
-	// Reloads the config with /cookme reload
-	private void CookMeReload(Player player) {
-		plugin.loadConfigsAgain();		
-		String message = plugin.localization.getString("reload");
-		plugin.message(player, message, null, null);
-	}
-
-	// Enables permissions with /cookme enable permissions
-	private void CookMeEnablePermissions(Player player) {
-		plugin.config.setBoolean("configuration.permissions", true);
-		plugin.config.save();
-		for (int i = 1; i <= 2; i++) {
-			String message = plugin.localization.getString("enable_permissions_" + Integer.toString(i));
-			plugin.message(player, message, null, null);
+	// Effect
+	if (Arrays.asList(plugin.effects).contains(args[1].toLowerCase())) {
+	    String effect = args[1].toLowerCase();
+	    if (args.length > 2) {
+		double percentage = 0.0;
+		try {
+		    percentage = Double.valueOf(args[2]);
 		}
-	}
-
-	// Disables permissions with /cookme disable permissions
-	private void CookMeDisablePermissions(Player player) {
-		plugin.config.setBoolean("configuration.permissions", false);
-		plugin.config.save();
-		for (int i = 1; i <= 2; i++) {
-			String message = plugin.localization.getString("disable_permissions_" + Integer.toString(i));
-			plugin.message(player, message, null, null);
+		// Percentage not a number?
+		catch (NumberFormatException e) {
+		    String message = plugin.localization.getString("no_number");
+		    plugin.message(player, message, null, null);
+		    return true;
 		}
-	}
-
-	// Enables messages with /cookme enable messages
-	private void CookMeEnableMessages(Player player) {
-		plugin.config.setBoolean("configuration.messages", true);
+		plugin.config.setDouble("effects." + effect.toLowerCase(), percentage);
 		plugin.config.save();
-		String message = plugin.localization.getString("enable_messages");
-		plugin.message(player, message, null, null);
+		String message = plugin.localization.getString("changed_effect");
+		plugin.message(player, message, effect, Double.toString(percentage));
+		return true;
+	    }
+	    else return false;
 	}
+	// enable
+	if (args.length > 0 && args[0].equalsIgnoreCase("enable")) {
+	    // permissions
+	    if (args.length > 1 && args[1].equalsIgnoreCase("permissions")) {
+		CookMeEnablePermissions(player);
+		return true;
+	    }
+	    // messages
+	    if (args.length > 1 && args[1].equalsIgnoreCase("messages")) {
+		CookMeEnableMessages(player);
+		return true;
+	    }
+	    else return false;
+	}
+	// disable
+	if (args.length > 0 && args[0].equalsIgnoreCase("disable")) {
+	    // permissions
+	    if (args.length > 1 && args[1].equalsIgnoreCase("permissions")) {
+		CookMeDisablePermissions(player);
+		return true;
+	    }
+	    // messages
+	    if (args.length > 1 && args[1].equalsIgnoreCase("messages")) {
+		CookMeDisableMessages(player);
+		return true;
+	    }
+	    else return false;
+	}
+	else {
+	    String message = plugin.localization.getString("permission_denied");
+	    plugin.message(player, message, null, null);
+	    return true;
+	}
+    }
 
-	// Disables messages with /cookme disable messages
-	private void CookMeDisableMessages(Player player) {
-		plugin.config.setBoolean("configuration.messages", false);
-		plugin.config.save();
-		String message = plugin.localization.getString("disable_messages");
-		plugin.message(player, message, null, null);
+    // See the help with /cookme help
+    private void CookMeHelp(Player player) {
+	for (int i = 1; i <= 11; i++) {
+	    String message = plugin.localization.getString("help_" + Integer.toString(i));
+	    plugin.message(player, message, null, null);
 	}
+    }
+
+    // Reloads the config with /cookme reload
+    private void CookMeReload(Player player) {
+	plugin.loadConfigsAgain();		
+	String message = plugin.localization.getString("reload");
+	plugin.message(player, message, null, null);
+    }
+
+    // Enables permissions with /cookme enable permissions
+    private void CookMeEnablePermissions(Player player) {
+	plugin.config.setBoolean("configuration.permissions", true);
+	plugin.config.save();
+	for (int i = 1; i <= 2; i++) {
+	    String message = plugin.localization.getString("enable_permissions_" + Integer.toString(i));
+	    plugin.message(player, message, null, null);
+	}
+    }
+
+    // Disables permissions with /cookme disable permissions
+    private void CookMeDisablePermissions(Player player) {
+	plugin.config.setBoolean("configuration.permissions", false);
+	plugin.config.save();
+	for (int i = 1; i <= 2; i++) {
+	    String message = plugin.localization.getString("disable_permissions_" + Integer.toString(i));
+	    plugin.message(player, message, null, null);
+	}
+    }
+
+    // Enables messages with /cookme enable messages
+    private void CookMeEnableMessages(Player player) {
+	plugin.config.setBoolean("configuration.messages", true);
+	plugin.config.save();
+	String message = plugin.localization.getString("enable_messages");
+	plugin.message(player, message, null, null);
+    }
+
+    // Disables messages with /cookme disable messages
+    private void CookMeDisableMessages(Player player) {
+	plugin.config.setBoolean("configuration.messages", false);
+	plugin.config.save();
+	String message = plugin.localization.getString("disable_messages");
+	plugin.message(player, message, null, null);
+    }
 }
--- src/main/java/CooldownManager.java
+++ src/main/java/CooldownManager.java
@@ -17,73 +17,73 @@ import java.util.LinkedHashMap;
  */
 
 public class CooldownManager {
-	private int cooldown = 0;
-	private LinkedHashMap<String, Timestamp> cooldownList = new LinkedHashMap<String, Timestamp>();
-	
-	// Sets the cooldown!
-	public CooldownManager(int cooldownValue) {
-		cooldown = cooldownValue;
+    private int cooldown = 0;
+    private LinkedHashMap<String, Timestamp> cooldownList = new LinkedHashMap<String, Timestamp>();
+
+    // Sets the cooldown!
+    public CooldownManager(int cooldownValue) {
+	cooldown = cooldownValue;
+    }
+
+    // Remember to set the cooldown!
+    public CooldownManager() {}
+
+    // Get the cooldown value
+    public int getCooldown() {
+	return cooldown;
+    }
+
+    // Set the cooldown value
+    public void setCooldown(int cooldownValue) {
+	cooldown = cooldownValue;
+    }
+
+    // Add the player with the now time to the hashmap
+    public boolean addPlayer(Player player) {
+	Timestamp time = new Timestamp(System.currentTimeMillis());
+	if (!cooldownList.containsKey(player.getName())) {
+	    cooldownList.put(player.getName(), time);
+	    return true;
 	}
-	
-	// Remember to set the cooldown!
-	public CooldownManager() {}
-	
-	// Get the cooldown value
-	public int getCooldown() {
-		return cooldown;
+	return false;
+    }
+
+    // Remove player
+    public boolean removePlayer(Player player) {
+	if (cooldownList.containsKey(player.getName())) {
+	    cooldownList.remove(player.getName());
+	    return true;
 	}
-	
-	// Set the cooldown value
-	public void setCooldown(int cooldownValue) {
-		cooldown = cooldownValue;
+	return false;
+    }
+
+    // Check for the cooldown
+    public boolean hasCooldown(Player player, Timestamp now) {
+	// If the player is on the list
+	if (cooldownList.containsKey(player.getName())) {
+	    Timestamp time = cooldownList.get(player.getName());
+	    long difference = (now.getTime()- time.getTime()) / 1000;
+	    // If the difference is bigger than the cooldown time -> no cooldown anymore
+	    if (difference > cooldown) removePlayer(player);
+	    else return true;
 	}
-	
-	// Add the player with the now time to the hashmap
-	public boolean addPlayer(Player player) {
-		Timestamp time = new Timestamp(System.currentTimeMillis());
-		if (!cooldownList.containsKey(player.getName())) {
-			cooldownList.put(player.getName(), time);
-			return true;
-		}
-		return false;
+	return false;
+    }
+
+    // Makes the list empty
+    public void clearCooldownList() {
+	cooldownList.clear();
+    }
+
+    // Returns the rest of the cooldown time
+    public long getRemainingCooldownTime(Player player, Timestamp now) {
+	if (cooldownList.containsKey(player.getName())) {
+	    Timestamp time = cooldownList.get(player.getName());
+	    long difference = (now.getTime()- time.getTime()) / 1000;
+	    // If the difference is bigger than the cooldown time -> no cooldown anymore
+	    if (difference > cooldown) return 0;
+	    else return difference;
 	}
-	
-	// Remove player
-	public boolean removePlayer(Player player) {
-		if (cooldownList.containsKey(player.getName())) {
-			cooldownList.remove(player.getName());
-			return true;
-		}
-		return false;
-	}
-	
-	// Check for the cooldown
-	public boolean hasCooldown(Player player, Timestamp now) {
-		// If the player is on the list
-		if (cooldownList.containsKey(player.getName())) {
-			Timestamp time = cooldownList.get(player.getName());
-			long difference = (now.getTime()- time.getTime()) / 1000;
-			// If the difference is bigger than the cooldown time -> no cooldown anymore
-			if (difference > cooldown) removePlayer(player);
-			else return true;
-		}
-		return false;
-	}
-	
-	// Makes the list empty
-	public void clearCooldownList() {
-		cooldownList.clear();
-	}
-	
-	// Returns the rest of the cooldown time
-	public long getRemainingCooldownTime(Player player, Timestamp now) {
-		if (cooldownList.containsKey(player.getName())) {
-			Timestamp time = cooldownList.get(player.getName());
-			long difference = (now.getTime()- time.getTime()) / 1000;
-			// If the difference is bigger than the cooldown time -> no cooldown anymore
-			if (difference > cooldown) return 0;
-			else return difference;
-		}
-		return 0;
-	}
+	return 0;
+    }
 }
